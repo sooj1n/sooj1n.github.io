53a8d976b4f60e04d6be0fc31d3ad32281995cb6
sooj1n <sujin19371@gmail.com>
2024-07-11 11:19:31 +0900

    change dt format

--- sudden.sh
+++ sudden.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 FILE=~/tmp/_SUDDEN
+DATE=$(date +"%Y%m%d %H:%M:%S")
 #MODEL=KIA
 
 while true
@@ -11,7 +12,7 @@ do
 	else 
 		clear
 		figlet KIA
-		date
+	        echo $DATE
 	fi
 	sleep 3
 #       date
